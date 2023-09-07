01e1d0e60a3a58b4536e0432cd4cd412eda223f5
Erin Wise <erinwise88@gmail.com>
2023-09-07 02:00:27 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+for (i=0; i < array.length; i++) {
+  //let j = i+1 //this is only increasing by 1 not looping through the rest of the array
+  for(j = i+1; j < array.length; j++ ) {
+  if(array[i] + array[j]=== target) return true
+ 
 }
+}
+return false
+}
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,11 +18,20 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
-*/
+  if the sum of a pair of any numbers in an array =  target , return true
+  otherwise return false
+  I want to loop through the array using a for loop 
+   - start with and i and j variable
+   - i = index 0
+   - let j start at the end of the array
+   - if i + j = target return true
+
 
 /*
   Add written explanation of your solution here
-*/
+  the outer loop iterates through each array element and for each element, the inner loop(j) iterates
+  through the remaining elements until the sum equals the target number
+*/ 
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
